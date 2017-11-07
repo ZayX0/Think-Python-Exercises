69752f4c53117a64c12e820bd513d0aad0adfc2f
Isaiah Reed <isaiah.codes@gmail.com>
2017-11-07 06:47:17 -0600

    Initial commit

---
+++ Exercise2.2/Book_price.py
@@ -0,0 +1,7 @@
+numBooks = 60
+storeBookPrice = numBooks * (24.95 * .6)
+shippingCosts = 3 + (.75 * (numBooks - 1))
+wholesaleCost = storeBookPrice + shippingCosts
+
+print(storeBookPrice)
+print(wholesaleCost)
---
+++ Exercise2.2/eat_breakfast.py
@@ -0,0 +1,5 @@
+startingTimeSec = (6*60+52)*60
+easyPaceSec = (8*60+15)*2
+hardPaceSec = (7*60+12)*3
+
+finish_hour_conversion = (startingTimeSec + easyPaceSec + hardPaceSec) / (60*60)
